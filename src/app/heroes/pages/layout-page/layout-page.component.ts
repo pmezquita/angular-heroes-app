1a3680a16d377b698a2cb54eb7aba68d1708ff06
pmezquita <ing.pedromezquita@gmail.com>
2023-05-10 12:39:41 -0600

    [ADD] NavDrawer

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class LayoutPageComponent {
-
+  sidebarItems = [
+    {label: 'listado', icon: 'label', url: './list'},
+    {label: 'Añadir', icon: 'add', url: './new-hero'},
+    {label: 'Buscar', icon: 'search', url: './search'},
+  ];
 }
