75d2cd9b73af930030e4afadc97235640c773460
pmezquita <ing.pedromezquita@gmail.com>
2023-05-10 11:23:31 -0600

    [ADD] LayoutPage

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,10 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import {LayoutPageComponent} from "./pages/layout-page/layout-page.component";
 import {NewPageComponent} from "./pages/new-page/new-page.component";
+import {SearchPageComponent} from "./pages/search-page/search-page.component";
+import {ListPageComponent} from "./pages/list-page/list-page.component";
+import {HeroPageComponent} from "./pages/hero-page/hero-page.component";
 
 // localhost:5000/heroes/
 const routes: Routes = [
@@ -9,17 +12,18 @@ const routes: Routes = [
     path: '',
     component: LayoutPageComponent,
     children: [
-      {
-        path: 'new-hero', component: NewPageComponent
-      },
+      {path: 'new-hero', component: NewPageComponent},
+      {path: 'search', component: SearchPageComponent},
+      {path: 'edit/:id', component: NewPageComponent},
+      {path: 'list', component: ListPageComponent},
+      {path: ':id', component: HeroPageComponent},
+      {path: '**', redirectTo: 'list'},
     ]
   },
-  {}
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HeroesRoutingModule {
-}
+export class HeroesRoutingModule { }
