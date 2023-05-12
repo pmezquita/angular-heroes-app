ef54a8cb19eb06c9bdfe840801f5cb5d6a362df9
pmezquita <ing.pedromezquita@gmail.com>
2023-05-12 12:35:48 -0600

    [UPD] FormControl en NewPage

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {Publisher} from "../../interfaces/hero.interface";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-new-page',
@@ -7,8 +8,20 @@ import {Publisher} from "../../interfaces/hero.interface";
   styles: []
 })
 export class NewPageComponent {
+
+  heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', {nonNullable: true}),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
+
   public publishers = [
     {id: 'DC Comics', desc: Publisher.DCComics},
     {id: 'Marvel Comics', desc: Publisher.MarvelComics},
   ];
+
 }
