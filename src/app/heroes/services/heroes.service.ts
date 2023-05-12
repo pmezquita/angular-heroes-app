f4c081a6d4e22776df920a6e833c7361d45503f4
pmezquita <ing.pedromezquita@gmail.com>
2023-05-12 12:04:39 -0600

    [UPD] Servicio CRUD

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {catchError, Observable, of} from "rxjs";
+import {catchError, map, Observable, of} from "rxjs";
 import {Hero} from "../interfaces/hero.interface";
 import {environment} from "../../../environments/environment";
 
@@ -22,6 +22,24 @@ export class HeroesService {
     return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
       .pipe(
         catchError(() => of(undefined))
+      );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw Error('Hero id is required');
+
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(() => of(false)),
+        map(() => true)
       );
   }
 
