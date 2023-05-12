bc5f17f89ad5f5b5b29c4fffefad84eeb7cc253c
pmezquita <ing.pedromezquita@gmail.com>
2023-05-12 11:40:54 -0600

    [ADD] Pantalla agregar heroe

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Publisher} from "../../interfaces/hero.interface";
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class NewPageComponent {
-
+  public publishers = [
+    {id: 'DC Comics', desc: Publisher.DCComics},
+    {id: 'Marvel Comics', desc: Publisher.MarvelComics},
+  ];
 }
