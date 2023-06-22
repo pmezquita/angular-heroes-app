a07c826b300b71d6ec82aa632c8036c27e4b6596
pmezquita <ing.pedromezquita@gmail.com>
2023-06-22 10:50:52 -0600

    [UPD] Actualizar y crear heroe

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
-import {Publisher} from "../../interfaces/hero.interface";
+import {Hero, Publisher} from "../../interfaces/hero.interface";
 import {FormControl, FormGroup} from "@angular/forms";
+import {HeroesService} from "../../services/heroes.service";
 
 @Component({
   selector: 'app-new-page',
@@ -8,6 +9,10 @@ import {FormControl, FormGroup} from "@angular/forms";
   styles: []
 })
 export class NewPageComponent {
+
+
+  constructor(private heroesService: HeroesService) {
+  }
 
   heroForm = new FormGroup({
     id: new FormControl(''),
@@ -19,16 +24,29 @@ export class NewPageComponent {
     alt_img: new FormControl(''),
   });
 
+  get currentHero(): Hero {
+    return this.heroForm.value as Hero;
+  }
+
   public publishers = [
     {id: 'DC Comics', desc: Publisher.DCComics},
     {id: 'Marvel Comics', desc: Publisher.MarvelComics},
   ];
 
   onSubmit(): void {
-    console.log({
-      formIsValid: this.heroForm.valid,
-      value: this.heroForm.value
-    })
+    if (this.heroForm.invalid) return;
+
+    if (this.currentHero.id) {
+      this.heroesService.updateHero(this.currentHero)
+        .subscribe(value => {
+          // TODO: mostrar snackbar
+        });
+    } else {
+      this.heroesService.addHero(this.currentHero)
+        .subscribe(value => {
+          // TODO: mostrar snackbar
+        });
+    }
   }
 
 }
