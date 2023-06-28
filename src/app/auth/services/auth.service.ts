d9c63893afd8b93477fc951e95c24a0f68cacefd
pmezquita <ing.pedromezquita@gmail.com>
2023-06-28 12:44:45 -0600

    [ADD] Autenticar y mostrar usuario activo

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -7,7 +7,7 @@ import {Observable, tap} from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
-export class ServiceService {
+export class AuthService {
 
   private baseUrl = environment.baseUrl;
   private user?: User;
@@ -22,10 +22,15 @@ export class ServiceService {
   login(email: string, password: string): Observable<User> {
     // http.post('login',{email, password});
 
-    return this.http.get<User>(`${this.baseUrl}/user/1`)
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
       .pipe(
         tap(user => this.user = user),
-        tap(user => localStorage.setItem('token', user.id.toString())),
+        tap(user => localStorage.setItem('token', 'asKJsd34k.jj24kjkJKJS.87asdasd')),
       );
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
   }
 }
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../../../auth/services/auth.service";
+import {User} from "../../../auth/interfaces/user.interface";
 
 @Component({
   selector: 'app-layout-page',
@@ -6,9 +9,24 @@ import {Component} from '@angular/core';
   styles: []
 })
 export class LayoutPageComponent {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
+  }
+
+  get user(): User | undefined {
+    return this.authService.currentUser;
+  }
+
   sidebarItems = [
     {label: 'listado', icon: 'label', url: './list'},
     {label: 'Añadir', icon: 'add', url: './new-hero'},
     {label: 'Buscar', icon: 'search', url: './search'},
   ];
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
 }
